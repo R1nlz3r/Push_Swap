905d57296e63fc4553c1d99314441030c8025fb4
Mathis Pandelé <mapandel@student.42.fr>
2017-05-12 18:47:28 +0200

    checker.h 1.1 : +Flags

--- includes/checker.h
+++ includes/checker.h
@@ -6,7 +6,7 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/04 12:44:10 by mapandel          #+#    #+#             */
-/*   Updated: 2017/05/06 19:57:06 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/05/12 18:31:24 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,54 @@
 #define CHECKER_H
 
 # include "../libft/libft.h"
+# include "fcntl.h"
 
 # define ERROR -1
 # define OK 0
 # define KO 1
 
+# define SA 0
+# define SB 1
+# define SS 2
+# define PA 3
+# define PB 4
+# define RA 5
+# define RB 6
+# define RR 7
+# define RRA 8
+# define RRB 9
+# define RRR 10
+
+typedef struct		t_checker_flags
+{
+	int				l;
+	int				s;
+	int				v;
+	int				c;
+}					t_checker_flags;
+
 typedef struct		s_checker
 {
+	t_checker_flags *flags;
 	int				*a;
 	int				*b;
-	size_t			a_size;
-	size_t			b_size;
+	int				a_size;
+	int				b_size;
+	int				rotation;
+	int				nbrotations;
+	int				argnb;
 	int				error;
-	char			pad_0[4];
 }					t_checker;
 
-t_checker			*checker_parsing(t_checker *ck, char **argv);
-int					checker_display(int result);
+t_checker			*checker_parsing_flags(t_checker *ck, int argc,
+	char **argv);
+t_checker			*checker_parsing_integers(t_checker *ck, int argc,
+	char **argv);
+void				checker_display_start(void);
+int					checker_display_usage(void);
+int					checker_display_result(int result);
+void				checker_display_rotation(t_checker *ck);
+void				checker_display_nbrotations(t_checker *ck);
+void				checker_display_stacks(t_checker *ck);
 
 #endif
