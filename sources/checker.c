3c4ed3321600bbe58cb1368311a5b299c2df92bb
Mathis Pandelé <mapandel@student.42.fr>
2017-05-12 18:48:25 +0200

    checker.c 1.1 : +Flags

--- sources/checker.c
+++ sources/checker.c
@@ -6,7 +6,7 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/04 12:47:12 by mapandel          #+#    #+#             */
-/*   Updated: 2017/05/06 20:09:53 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/05/12 18:47:50 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,19 @@ static int			del_t_checker(t_checker *ck)
 static t_checker	*init_t_checker(t_checker *ck, int argc)
 {
 	if (!(ck = ft_memalloc(sizeof(t_checker)))
+		|| !(ck->flags = ft_memalloc(sizeof(t_checker_flags)))
 		|| !(ck->a = ft_tabnew(((size_t)(argc - 1))))
 		|| !(ck->b = ft_tabnew(0)))
 		return (NULL);
-	ck->a_size = (size_t)(argc - 1);
+	ck->flags->l = 0;
+	ck->flags->s = 10000;
+	ck->flags->v = 0;
+	ck->flags->c = 0;
+	ck->argnb = 1;
+	ck->a_size = 0;
 	ck->b_size = 0;
+	ck->rotation = 0;
+	ck->nbrotations = 0;
 	ck->error = 0;
 	return (ck);
 }
@@ -38,14 +46,18 @@ int					main(int argc, char **argv)
 
 	ck = NULL;
 	if (argc < 2)
-		return (checker_display(KO));
+		return (checker_display_usage());
 	if (!(ck = init_t_checker(ck, argc)) && del_t_checker(ck))
-		return (checker_display(KO));
-	ck = checker_parsing(ck, argv);
+		return (checker_display_result(ERROR));
+	checker_parsing_flags(ck, argc, argv);
+	if (!(ck->a_size = argc - ck->argnb))
+		ck->error = -1;
 	if (ck->error && del_t_checker(ck))
-		return (checker_display(KO));
-	//resolve
+		return (checker_display_result(ERROR));
+	checker_parsing_integers(ck, argc, argv);
+	if (ck->error && del_t_checker(ck))
+		return (checker_display_result(ERROR));
 	del_t_checker(ck);
-	checker_display(OK);
+	checker_display_result(OK);
 	return (0);
 }
