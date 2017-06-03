d1d107840c24264c4b68bff1917b092851e4b0bb
Mathis Pandelé <mapandel@student.42.fr>
2017-06-03 03:37:36 +0200

    checker_display2.c 1.1 : Arrangement for t_tab / Working

--- sources/checker_display2.c
+++ sources/checker_display2.c
@@ -6,20 +6,21 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/12 16:18:44 by mapandel          #+#    #+#             */
-/*   Updated: 2017/05/12 17:50:16 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/06/03 03:19:15 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "checker.h"
 
-void		checker_display_nbrotations(t_checker *ck)
+static void		checker_display_nbrotations(t_checker *ck)
 {
+	ft_putstr("\033c");
 	ft_putstr("\033[4mThe number of rotations\033[24m : ");
 	ft_putnbr(ck->nbrotations);
 	ft_putchar('\n');
 }
 
-void		checker_display_rotation(t_checker *ck)
+static void		checker_display_rotation(t_checker *ck)
 {
 	ft_putstr("\033[4mLast rotation\033[24m : ");
 	if (ck->rotation == SA)
@@ -47,29 +48,36 @@ void		checker_display_rotation(t_checker *ck)
 	ft_putchar('\n');
 }
 
-void		checker_display_stacks(t_checker *ck)
+static void		checker_display_stacks(t_checker *ck)
 {
-	int		i;
+	size_t		i;
 
 	i = 0;
 	ft_putendl("\n\033[4mStacks\033[24m :");
 	ft_putendl("/*");
 	ft_putstr("** \033[7ma\033[27m");
-	while (i < ck->a_size)
+	while (i < ck->a->len)
 	{
 		ft_putstr(" ");
-		ft_putnbr(ck->a[i]);
+		ft_putnbr(ck->a->tab[i]);
 		++i;
 	}
 	ft_putchar('\n');
 	ft_putstr("** \033[7mb\033[27m");
 	i = 0;
-	while (i < ck->b_size)
+	while (i < ck->b->len)
 	{
 		ft_putstr(" ");
-		ft_putnbr(ck->b[i]);
+		ft_putnbr(ck->b->tab[i]);
 		++i;
 	}
 	ft_putchar('\n');
 	ft_putendl("*/\n");
+}
+
+void			checker_display_v_flag(t_checker *ck)
+{
+	checker_display_nbrotations(ck);
+	checker_display_rotation(ck);
+	checker_display_stacks(ck);
 }
