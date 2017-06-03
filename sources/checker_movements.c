70fc55ed7da61a568979f212bed0969a5919aa15
Mathis Pandelé <mapandel@student.42.fr>
2017-06-03 03:38:14 +0200

    checker_movements.c 1.0 : Implementation

---
+++ sources/checker_movements.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   checker_movements.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/05/22 18:31:16 by mapandel          #+#    #+#             */
+/*   Updated: 2017/06/03 00:13:56 by mapandel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "checker.h"
+
+t_checker		*checker_movements(t_checker *ck)
+{
+	if (ck->flags->l && (((ck->fd = open("log.txt", O_RDONLY)) < 0)
+		|| read(ck->fd, 0, 0)) && (ck->error = -1))
+		return (ck);
+	if (ck->flags->v)
+		checker_display_v_flag(ck);
+	while (get_next_line(ck->fd, &ck->line) > 0)
+	{
+		if (ck->flags->v)
+			usleep((useconds_t)ck->flags->s);
+		ck = checker_parsing_movements(ck);
+		if (ck->error || !ck->line[0])
+		{
+			ft_strdel(&ck->line);
+			break ;
+		}
+		if (ck->flags->v && ++ck->nbrotations)
+			checker_display_v_flag(ck);
+		ft_strdel(&ck->line);
+	}
+	if (ck->flags->l && close(ck->fd) == -1)
+		ck->error = -1;
+	if (!ft_tabissort(ck->a) || ck->b->len)
+		ck->error = 1;
+	return (ck);
+}
