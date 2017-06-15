616f6cb37cb4b5d52c61aa7c18b1b79eff894015
Mathis Pandelé <mapandel@student.42.fr>
2017-06-15 05:11:52 +0200

    push_swap.h 1.1 : Working

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,14 +6,30 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/03 17:47:39 by mapandel          #+#    #+#             */
-/*   Updated: 2017/06/04 21:56:31 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/06/07 21:28:36 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
+# include <stdio.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
 # include "libft.h"
+
+# define SA 1
+# define SB 2
+# define SS 3
+# define PA 4
+# define PB 5
+# define RA 6
+# define RB 7
+# define RR 8
+# define RRA 9
+# define RRB 10
+# define RRR 11
 
 typedef struct		s_push_swap
 {
@@ -21,11 +37,16 @@ typedef struct		s_push_swap
 	t_tab			*b;
 	int				l;
 	int				argnb;
+	int				fd;
+	int				median;
 }					t_push_swap;
 
 t_push_swap			*push_swap_parsing_flags(t_push_swap *ps, int argc,
 	char **argv);
 t_push_swap			*push_swap_parsing_integers(t_push_swap *ps, int argc,
 	char **argv);
+t_push_swap			*push_swap_solver(t_push_swap *ps);
+void				push_swap_display_usage(void) __attribute__ ((noreturn));
+void				push_swap_display_movements(t_push_swap *ps, int mv);
 
 #endif
