4472600d6f80439ddc6faf28e0c4b4862ac55e3e
Mathis Pandelé <mapandel@student.42.fr>
2017-06-24 06:22:30 +0200

    push_swap_display.c 1.1 : Movements auto

--- sources/push_swap_display.c
+++ sources/push_swap_display.c
@@ -6,7 +6,7 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/05 02:05:37 by mapandel          #+#    #+#             */
-/*   Updated: 2017/06/05 19:06:38 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/06/23 15:21:45 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,31 @@
 
 void		push_swap_display_movements(t_push_swap *ps, int mv)
 {
-	if (mv == SA)
+	if (mv == SA && (ft_tabswap(ps->a)))
 		ft_putendl_fd("sa", ps->fd);
-	else if (mv == SB)
+	else if (mv == SB && (ft_tabswap(ps->b)))
 		ft_putendl_fd("sb", ps->fd);
-	else if (mv == SS)
+	else if (mv == SS && (ft_tabswap(ps->a)) && (ft_tabswap(ps->b)))
 		ft_putendl_fd("ss", ps->fd);
 	else if (mv == PA)
+		ft_tabpush(ps->b, ps->a);
+	if (mv == PA)
 		ft_putendl_fd("pa", ps->fd);
 	else if (mv == PB)
+		ft_tabpush(ps->a, ps->b);
+	if (mv == PB)
 		ft_putendl_fd("pb", ps->fd);
-	else if (mv == RA)
+	else if (mv == RA && (ft_tabrotate(ps->a)))
 		ft_putendl_fd("ra", ps->fd);
-	else if (mv == RB)
+	else if (mv == RB && (ft_tabrotate(ps->b)))
 		ft_putendl_fd("rb", ps->fd);
-	else if (mv == RR)
+	else if (mv == RR && (ft_tabrotate(ps->a)) && (ft_tabrotate(ps->b)))
 		ft_putendl_fd("rr", ps->fd);
-	else if (mv == RRA)
+	else if (mv == RRA && (ft_tabrevrotate(ps->a)))
 		ft_putendl_fd("rra", ps->fd);
-	else if (mv == RRB)
+	else if (mv == RRB && (ft_tabrevrotate(ps->b)))
 		ft_putendl_fd("rrb", ps->fd);
-	else if (mv == RRR)
+	else if (mv == RRR && (ft_tabrevrotate(ps->a)) && (ft_tabrevrotate(ps->b)))
 		ft_putendl_fd("rrr", ps->fd);
 }
 
