a6fff10db06134773b57d8611a4154081ec4fb45
Mathis Pandelé <mapandel@student.42.fr>
2017-06-24 06:21:23 +0200

    push_swap_solve.c 1.2 : Different algorithms added

--- sources/push_swap_solve.c
+++ sources/push_swap_solve.c
@@ -6,11 +6,127 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/05 04:23:18 by mapandel          #+#    #+#             */
-/*   Updated: 2017/06/16 23:12:05 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/06/24 06:19:14 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+/*
+if (ps->a->len >= 2 && ps->b->len >= 2
+	&& ps->a->tab[ps->a->len - 2] < ps->a->tab[ps->a->len - 1]
+	&& ps->b->tab[ps->b->len - 2] > ps->b->tab[ps->b->len - 1])
+	push_swap_display_movements(ps, SS);
+else if (ps->b->len >= 2
+	&& ps->b->tab[ps->b->len - 2] > ps->b->tab[ps->b->len - 1])
+	push_swap_display_movements(ps, SB);
+else if (ft_tabisdescending(ps->a) && ps->b->tab[ps->b->len - 1]
+	< ps->a->tab[ps->a->len - 1])
+	push_swap_display_movements(ps, PA);
+else if ((ft_tabmin(ps->a) == ps->a->tab[ps->a->len - 1])
+	|| (ft_tabmin_overone(ps->a) == ps->a->tab[ps->a->len - 1]))
+	push_swap_display_movements(ps, PB);
+else if ((ft_tabminpos(ps->a) < ps->a->len / 2
+	|| ft_tabminpos_overone(ps->a) < ps->a->len / 2))
+	push_swap_display_movements(ps, RRA);
+else
+	push_swap_display_movements(ps, RA);
+
+	500nb -> 27289
+*/
+
+/*
+if ((ft_tabisdescending(ps->a) || !ps->a->len) && ft_tabisincreasing(ps->b) && (!ps->a->len || ps->a->tab[ps->a->len - 1] > ps->b->tab[ps->b->len - 1]))
+	push_swap_display_movements(ps, PA);
+else if ((ft_tabmin(ps->a) <= ps->median / 4 && ps->a->tab[ps->a->len - 1] > ps->median / 4)
+	|| (ft_tabmin(ps->a) <= (ps->median / 4 + ps->median / 8) && ps->a->tab[ps->a->len - 1] > (ps->median / 4 + ps->median / 8))
+	|| (ft_tabmin(ps->a) <= ps->median / 2 && ps->a->tab[ps->a->len - 1] > ps->median / 2)
+	|| (ft_tabmin(ps->a) <= (ps->median / 2 + ps->median / 8) && ps->a->tab[ps->a->len - 1] > (ps->median / 2 + ps->median / 8))
+	|| (ft_tabmin(ps->a) <= (ps->median / 2 + ps->median / 4) && ps->a->tab[ps->a->len - 1] > (ps->median / 2 + ps->median / 4))
+	|| (ft_tabmin(ps->a) <= (ps->median / 2 + ps->median / 4 + ps->median / 8) && ps->a->tab[ps->a->len - 1] > (ps->median / 2 + ps->median / 4 + ps->median / 8))
+	|| (ft_tabmin(ps->a) <= ps->median && ps->a->tab[ps->a->len - 1] > ps->median)
+	|| (ft_tabmin(ps->a) <= (ps->median + ps->median / 8) && ps->a->tab[ps->a->len - 1] > (ps->median + ps->median / 8))
+	|| (ft_tabmin(ps->a) <= (ps->median + ps->median / 4) && ps->a->tab[ps->a->len - 1] > (ps->median + ps->median / 4))
+	|| (ft_tabmin(ps->a) <= (ps->median + ps->median / 4 + ps->median / 8) && ps->a->tab[ps->a->len - 1] > (ps->median + ps->median / 4 + ps->median / 8))
+	|| (ft_tabmin(ps->a) <= (ps->median + ps->median / 2) && ps->a->tab[ps->a->len - 1] > (ps->median + ps->median / 2))
+	|| (ft_tabmin(ps->a) <= (ps->median + ps->median / 2 + ps->median / 8) && ps->a->tab[ps->a->len - 1] > (ps->median + ps->median / 2 + ps->median / 8))
+	|| (ft_tabmin(ps->a) <= (ps->median + ps->median / 2 + ps->median / 4) && ps->a->tab[ps->a->len - 1] > (ps->median + ps->median / 2 + ps->median / 4))
+	|| (ft_tabmin(ps->a) <= (ps->median + ps->median / 2 + ps->median / 4 + ps->median / 8) && ps->a->tab[ps->a->len - 1] > (ps->median + ps->median / 2 + ps->median / 4 + ps->median / 8)))
+	push_swap_display_movements(ps, RA);
+else if (ps->a->len && ((ft_tabmax(ps->a) > ft_tabmin(ps->b) || ft_tabmin(ps->a) < ft_tabmax(ps->b)) || ps->b->len <= 1) && (ps->b->len <= 1
+	|| ((ps->a->tab[ps->a->len - 1] < ft_tabmin(ps->b) || ps->a->tab[ps->a->len - 1] > ft_tabmax(ps->b)) && ps->b->tab[ps->b->len - 1] == ft_tabmax(ps->b))
+	|| (ps->a->tab[ps->a->len - 1] > ps->b->tab[ps->b->len - 1] && (!ps->b->len || ps->a->tab[ps->a->len - 1] < ps->b->tab[0]))))
+	push_swap_display_movements(ps, PB);
+else if ((!ps->a->len && ft_tabmaxpos(ps->b) < ps->b->len / 2)
+	|| (ps->a->tab[ps->a->len - 1] < ft_tabmedian(ps->b) && ft_tabmaxpos(ps->b) >= ps->b->len / 2 && ps->a->tab[ps->a->len - 1] > ps->b->tab[ps->b->len - 1])
+	|| (ps->a->tab[ps->a->len - 1] > ft_tabmedian(ps->b) && ft_tabmaxpos(ps->b) < ps->b->len / 2 && ps->a->tab[ps->a->len - 1] > ps->b->tab[0]))
+	push_swap_display_movements(ps, RRB);
+else
+	push_swap_display_movements(ps, RB);
+
+	500nb -> 7273
+*/
+
+/*
+if ((ft_tabisdescending(ps->a) || !ps->a->len) && ft_tabisincreasing(ps->b) && (!ps->a->len || ps->a->tab[ps->a->len - 1] > ps->b->tab[ps->b->len - 1]))
+	push_swap_display_movements(ps, PA);
+else if ((ft_tabmin(ps->a) <= ps->median / 4 && ft_tabfindrev_undereq(ps->a, ps->median / 4)
+	&& ft_tabfindpos_undereq(ps->a, ps->median / 4) <= ft_tabfindrev_undereq(ps->a, ps->median / 4))
+	|| (ft_tabmin(ps->a) <= (ps->median / 4 + ps->median / 8) && ft_tabmin(ps->a) > ps->median / 4 && ft_tabfindrev_undereq(ps->a, ps->median / 4 + ps->median / 8)
+	&& ft_tabfindpos_undereq(ps->a, ps->median / 4 + ps->median / 8) <= ft_tabfindrev_undereq(ps->a, ps->median / 4 + ps->median / 8))
+	|| (ft_tabmin(ps->a) <= ps->median / 2 && ft_tabmin(ps->a) > (ps->median / 4 + ps->median / 8) && ft_tabfindrev_undereq(ps->a, ps->median / 2)
+	&& ft_tabfindpos_undereq(ps->a, ps->median / 2) <= ft_tabfindrev_undereq(ps->a, ps->median / 2))
+	|| (ft_tabmin(ps->a) <= (ps->median / 2 + ps->median / 8) && ft_tabmin(ps->a) > ps->median / 2 && ft_tabfindrev_undereq(ps->a, ps->median / 2 + ps->median / 8)
+	&& ft_tabfindpos_undereq(ps->a, ps->median / 2 + ps->median / 8) <= ft_tabfindrev_undereq(ps->a, ps->median / 2 + ps->median / 8))
+	|| (ft_tabmin(ps->a) <= (ps->median / 2 + ps->median / 4) && ft_tabmin(ps->a) > (ps->median / 2 + ps->median / 8) && ft_tabfindrev_undereq(ps->a, ps->median / 2 + ps->median / 4)
+	&& ft_tabfindpos_undereq(ps->a, ps->median / 2 + ps->median / 4) <= ft_tabfindrev_undereq(ps->a, ps->median / 2 + ps->median / 4))
+	|| (ft_tabmin(ps->a) <= (ps->median / 2 + ps->median / 4 + ps->median / 8) && ft_tabmin(ps->a) > (ps->median / 2 + ps->median / 4) && ft_tabfindrev_undereq(ps->a, ps->median / 2 + ps->median / 4 + ps->median / 8)
+	&& ft_tabfindpos_undereq(ps->a, ps->median / 2 + ps->median / 4 + ps->median / 8) <= ft_tabfindrev_undereq(ps->a, ps->median / 2 + ps->median / 4 + ps->median / 8))
+	|| (ft_tabmin(ps->a) <= ps->median && ft_tabmin(ps->a) > (ps->median / 2 + ps->median / 4 + ps->median / 8) && ft_tabfindrev_undereq(ps->a, ps->median)
+	&& ft_tabfindpos_undereq(ps->a, ps->median) <= ft_tabfindrev_undereq(ps->a, ps->median))
+	|| (ft_tabmin(ps->a) <= (ps->median + ps->median / 8) && ft_tabmin(ps->a) > ps->median && ft_tabfindrev_undereq(ps->a, ps->median + ps->median / 8)
+	&& ft_tabfindpos_undereq(ps->a, ps->median + ps->median / 8) <= ft_tabfindrev_undereq(ps->a, ps->median + ps->median / 8))
+	|| (ft_tabmin(ps->a) <= (ps->median + ps->median / 4) && ft_tabmin(ps->a) > (ps->median + ps->median / 8) && ft_tabfindrev_undereq(ps->a, ps->median + ps->median / 4)
+	&& ft_tabfindpos_undereq(ps->a, ps->median + ps->median / 4) <= ft_tabfindrev_undereq(ps->a, ps->median + ps->median / 4))
+	|| (ft_tabmin(ps->a) <= (ps->median + ps->median / 4 + ps->median / 8) && ft_tabmin(ps->a) > (ps->median + ps->median / 4) && ft_tabfindrev_undereq(ps->a, ps->median + ps->median / 4 + ps->median / 8)
+	&& ft_tabfindpos_undereq(ps->a, ps->median + ps->median / 4 + ps->median / 8) <= ft_tabfindrev_undereq(ps->a, ps->median + ps->median / 4 + ps->median / 8))
+	|| (ft_tabmin(ps->a) <= (ps->median + ps->median / 2) && ft_tabmin(ps->a) > (ps->median + ps->median / 4 + ps->median / 8) && ft_tabfindrev_undereq(ps->a, ps->median + ps->median / 2)
+	&& ft_tabfindpos_undereq(ps->a, ps->median + ps->median / 2) <= ft_tabfindrev_undereq(ps->a, ps->median + ps->median / 2))
+	|| (ft_tabmin(ps->a) <= (ps->median + ps->median / 2 + ps->median / 8) && ft_tabmin(ps->a) > (ps->median + ps->median / 2) && ft_tabfindrev_undereq(ps->a, ps->median + ps->median / 2 + ps->median / 8)
+	&& ft_tabfindpos_undereq(ps->a, ps->median + ps->median / 2 + ps->median / 8) <= ft_tabfindrev_undereq(ps->a, ps->median + ps->median / 2 + ps->median / 8))
+	|| (ft_tabmin(ps->a) <= (ps->median + ps->median / 2 + ps->median / 4) && ft_tabmin(ps->a) > (ps->median + ps->median / 2 + ps->median / 8) && ft_tabfindrev_undereq(ps->a, ps->median + ps->median / 2 + ps->median / 4)
+	&& ft_tabfindpos_undereq(ps->a, ps->median + ps->median / 2 + ps->median / 4) <= ft_tabfindrev_undereq(ps->a, ps->median + ps->median / 2 + ps->median / 4))
+	|| (ft_tabmin(ps->a) <= (ps->median + ps->median / 2 + ps->median / 4 + ps->median / 8) && ft_tabmin(ps->a) > (ps->median + ps->median / 2 + ps->median / 4) && ft_tabfindrev_undereq(ps->a, ps->median + ps->median / 2 + ps->median / 4 + ps->median / 8)
+	&& ft_tabfindpos_undereq(ps->a, ps->median + ps->median / 2 + ps->median / 4 + ps->median / 8) <= ft_tabfindrev_undereq(ps->a, ps->median + ps->median / 2 + ps->median / 4 + ps->median / 8)))
+	push_swap_display_movements(ps, RRA);
+else if ((ft_tabmin(ps->a) <= ps->median / 4 && ft_tabfindrev_undereq(ps->a, ps->median / 4))
+	|| (ft_tabmin(ps->a) <= (ps->median / 4 + ps->median / 8) && ft_tabmin(ps->a) > ps->median / 4 && ft_tabfindrev_undereq(ps->a, ps->median / 4 + ps->median / 8))
+	|| (ft_tabmin(ps->a) <= ps->median / 2 && ft_tabmin(ps->a) > (ps->median / 4 + ps->median / 8) && ft_tabfindrev_undereq(ps->a, ps->median / 2))
+	|| (ft_tabmin(ps->a) <= (ps->median / 2 + ps->median / 8) && ft_tabmin(ps->a) > ps->median / 2 && ft_tabfindrev_undereq(ps->a, ps->median / 2 + ps->median / 8))
+	|| (ft_tabmin(ps->a) <= (ps->median / 2 + ps->median / 4) && ft_tabmin(ps->a) > (ps->median / 2 + ps->median / 8) && ft_tabfindrev_undereq(ps->a, ps->median / 2 + ps->median / 4))
+	|| (ft_tabmin(ps->a) <= (ps->median / 2 + ps->median / 4 + ps->median / 8) && ft_tabmin(ps->a) > (ps->median / 2 + ps->median / 4) && ft_tabfindrev_undereq(ps->a, ps->median / 2 + ps->median / 4 + ps->median / 8))
+	|| (ft_tabmin(ps->a) <= ps->median && ft_tabmin(ps->a) > (ps->median / 2 + ps->median / 4 + ps->median / 8) && ft_tabfindrev_undereq(ps->a, ps->median))
+	|| (ft_tabmin(ps->a) <= (ps->median + ps->median / 8) && ft_tabmin(ps->a) > ps->median && ft_tabfindrev_undereq(ps->a, ps->median + ps->median / 8))
+	|| (ft_tabmin(ps->a) <= (ps->median + ps->median / 4) && ft_tabmin(ps->a) > (ps->median + ps->median / 8) && ft_tabfindrev_undereq(ps->a, ps->median + ps->median / 4))
+	|| (ft_tabmin(ps->a) <= (ps->median + ps->median / 4 + ps->median / 8) && ft_tabmin(ps->a) > (ps->median + ps->median / 4) && ft_tabfindrev_undereq(ps->a, ps->median + ps->median / 4 + ps->median / 8))
+	|| (ft_tabmin(ps->a) <= (ps->median + ps->median / 2) && ft_tabmin(ps->a) > (ps->median + ps->median / 4 + ps->median / 8) && ft_tabfindrev_undereq(ps->a, ps->median + ps->median / 2))
+	|| (ft_tabmin(ps->a) <= (ps->median + ps->median / 2 + ps->median / 8) && ft_tabmin(ps->a) > (ps->median + ps->median / 2) && ft_tabfindrev_undereq(ps->a, ps->median + ps->median / 2 + ps->median / 8))
+	|| (ft_tabmin(ps->a) <= (ps->median + ps->median / 2 + ps->median / 4) && ft_tabmin(ps->a) > (ps->median + ps->median / 2 + ps->median / 8) && ft_tabfindrev_undereq(ps->a, ps->median + ps->median / 2 + ps->median / 4))
+	|| (ft_tabmin(ps->a) <= (ps->median + ps->median / 2 + ps->median / 4 + ps->median / 8) && ft_tabmin(ps->a) > (ps->median + ps->median / 2 + ps->median / 4) && ft_tabfindrev_undereq(ps->a, ps->median + ps->median / 2 + ps->median / 4 + ps->median / 8)))
+	push_swap_display_movements(ps, RA);
+else if (ps->a->len && ((ft_tabmax(ps->a) > ft_tabmin(ps->b) || ft_tabmin(ps->a) < ft_tabmax(ps->b)) || ps->b->len <= 1) && (ps->b->len <= 1
+	|| ((ps->a->tab[ps->a->len - 1] < ft_tabmin(ps->b) || ps->a->tab[ps->a->len - 1] > ft_tabmax(ps->b)) && ps->b->tab[ps->b->len - 1] == ft_tabmax(ps->b))
+	|| (ps->a->tab[ps->a->len - 1] > ps->b->tab[ps->b->len - 1] && (!ps->b->len || ps->a->tab[ps->a->len - 1] < ps->b->tab[0]))))
+	push_swap_display_movements(ps, PB);
+else if ((!ps->a->len && ft_tabmaxpos(ps->b) < ps->b->len / 2)
+	|| (ps->a->tab[ps->a->len - 1] < ft_tabmedian(ps->b) && ft_tabmaxpos(ps->b) >= ps->b->len / 2 && ps->a->tab[ps->a->len - 1] > ps->b->tab[ps->b->len - 1])
+	|| (ps->a->tab[ps->a->len - 1] > ft_tabmedian(ps->b) && ft_tabmaxpos(ps->b) < ps->b->len / 2 && ps->a->tab[ps->a->len - 1] > ps->b->tab[0]))
+	push_swap_display_movements(ps, RRB);
+else
+	push_swap_display_movements(ps, RB);
+
+	500nb -> 7776
+*/
 
 t_push_swap		*push_swap_solver(t_push_swap *ps)
 {
@@ -20,33 +136,7 @@ t_push_swap		*push_swap_solver(t_push_swap *ps)
 	ps->median = (int)(ft_tabmedian(ps->a));
 	while (!ft_tabisdescending(ps->a) || ps->b->len)
 	{
-		if (ps->a->len >= 2 && ps->b->len >= 2
-			&& ps->a->tab[ps->a->len - 2] < ps->a->tab[ps->a->len - 1]
-			&& ps->b->tab[ps->b->len - 2] > ps->b->tab[ps->b->len - 1]
-			&& (ps->a = ft_tabswap(ps->a)) && (ps->b = ft_tabswap(ps->b)))
-			push_swap_display_movements(ps, SS);
-		else if (ps->b->len >= 2
-			&& ps->b->tab[ps->b->len - 2] > ps->b->tab[ps->b->len - 1]
-			&& (ps->b = ft_tabswap(ps->b)))
-			push_swap_display_movements(ps, SB);
-		else if (ft_tabisdescending(ps->a) && ps->b->tab[ps->b->len - 1]
-			< ps->a->tab[ps->a->len - 1])
-		{
-			ft_tabpush(ps->b, ps->a);
-			push_swap_display_movements(ps, PA);
-		}
-		else if ((ft_tabmin(ps->a) == ps->a->tab[ps->a->len - 1])
-			|| (ft_tabmin_overone(ps->a) == ps->a->tab[ps->a->len - 1]))
-		{
-			ft_tabpush(ps->a, ps->b);
-			push_swap_display_movements(ps, PB);
-		}
-		else if ((ft_tabminpos(ps->a) < ps->a->len / 2
-			|| ft_tabminpos_overone(ps->a) < ps->a->len / 2)
-			&& (ps->a = ft_tabrevrotate(ps->a)))
-			push_swap_display_movements(ps, RRA);
-		else if ((ps->a = ft_tabrotate(ps->a)))
-			push_swap_display_movements(ps, RA);
+		//Choose your algorithm
 	}
 	if (ps->l && close(ps->fd))
 		exit (-1);
