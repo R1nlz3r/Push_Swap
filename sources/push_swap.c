547b24e90a82ed73b7bfe4c98d13ec7a0606a6e7
Mathis Pandelé <mapandel@student.42.fr>
2017-08-01 13:57:04 +0200

    push_swap.c 1.2 : Parsing bash arg

--- sources/push_swap.c
+++ sources/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/03 17:46:54 by mapandel          #+#    #+#             */
-/*   Updated: 2017/06/07 16:42:04 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/07/26 06:17:05 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,18 @@ static void				del_t_push_swap(t_push_swap *ps)
 	ft_memdel((void**)&ps);
 }
 
-static t_push_swap		*init_t_push_swap(t_push_swap *ps, int argc)
+static t_push_swap		*init_t_push_swap(t_push_swap *ps)
 {
 	if (!(ps = ft_memalloc(sizeof(t_push_swap)))
-		|| !(ps->a = ft_tabnew((size_t)(argc - 1)))
-		|| !(ps->b = ft_tabnew((size_t)(argc - 1))))
+		|| !(ps->a = ft_tabnew(1000000))
+		|| !(ps->b = ft_tabnew(1000000)))
 		exit(-1);
 	ps->l = 0;
 	ps->argnb = 1;
 	ps->fd = 1;
 	ps->median = 0;
+	ps->lastmove = 0;
+	ps->lastmove_count = 0;
 	return (ps);
 }
 
@@ -39,11 +41,16 @@ int		main(int argc, char **argv)
 	ps = NULL;
 	if (argc < 2)
 		push_swap_display_usage();
-	ps = init_t_push_swap(ps, argc);
+	ps = init_t_push_swap(ps);
 	ps = push_swap_parsing_flags(ps, argc, argv);
 	if (!(ps->a->len = (size_t)(argc - ps->argnb)))
 		exit(-1);
-	ps = push_swap_parsing_integers(ps, argc, argv);
+	if (ps->a->len == 1)
+		ps = push_swap_parsing_bash_arg(ps, argv);
+	else
+		ps = push_swap_parsing_integers(ps, argc, argv);
+	push_swap_parsing_doubles(ps);
+	ps->a = ft_tabrev_leakless(ps->a);
 	ps = push_swap_solver(ps);
 	del_t_push_swap(ps);
 	return (0);
