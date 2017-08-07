6af2b834106157db3965bfd38d377a2fd55822b4
Mathis Pandelé <mapandel@student.42.fr>
2017-08-07 12:38:55 +0200

    checker_display2.c 1.2 : Buffering

--- sources/checker_display2.c
+++ sources/checker_display2.c
@@ -6,7 +6,7 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/12 16:18:44 by mapandel          #+#    #+#             */
-/*   Updated: 2017/06/06 04:25:12 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/08/07 12:33:02 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,38 +14,39 @@
 
 static void		checker_display_nbrotations(t_checker *ck)
 {
-	ft_putstr("\033c");
-	ft_putstr("\033[4mThe number of rotations\033[24m : ");
-	ft_putnbr(ck->nbrotations);
-	ft_putchar('\n');
+	//\033[?25l Hide Cursor
+	ft_putstr_buffer("\033[H\033[J", 0);
+	ft_putstr_buffer("\033[4mThe number of rotations\033[24m : ", 0);
+	ft_putllnbr_buffer(ck->nbrotations, 0);
+	ft_putchar_buffer('\n', 0);
 }
 
 static void		checker_display_rotation(t_checker *ck)
 {
-	ft_putstr("\033[4mLast rotation\033[24m : ");
+	ft_putstr_buffer("\033[4mLast rotation\033[24m : ", 0);
 	if (ck->rotation == SA)
-		ft_putstr("sa");
+		ft_putstr_buffer("sa", 0);
 	else if (ck->rotation == SB)
-		ft_putstr("sb");
+		ft_putstr_buffer("sb", 0);
 	else if (ck->rotation == SS)
-		ft_putstr("ss");
+		ft_putstr_buffer("ss", 0);
 	else if (ck->rotation == PA)
-		ft_putstr("pa");
+		ft_putstr_buffer("pa", 0);
 	else if (ck->rotation == PB)
-		ft_putstr("pb");
+		ft_putstr_buffer("pb", 0);
 	else if (ck->rotation == RA)
-		ft_putstr("ra");
+		ft_putstr_buffer("ra", 0);
 	else if (ck->rotation == RB)
-		ft_putstr("rb");
+		ft_putstr_buffer("rb", 0);
 	else if (ck->rotation == RR)
-		ft_putstr("rr");
+		ft_putstr_buffer("rr", 0);
 	else if (ck->rotation == RRA)
-		ft_putstr("rra");
+		ft_putstr_buffer("rra", 0);
 	else if (ck->rotation == RRB)
-		ft_putstr("rrb");
+		ft_putstr_buffer("rrb", 0);
 	else if (ck->rotation == RRR)
-		ft_putstr("rrr");
-	ft_putchar('\n');
+		ft_putstr_buffer("rrr", 0);
+	ft_putchar_buffer('\n', 0);
 }
 
 static void		checker_display_stacks(t_checker *ck)
@@ -53,26 +54,26 @@ static void		checker_display_stacks(t_checker *ck)
 	size_t		i;
 
 	i = 0;
-	ft_putendl("\n\033[4mStacks\033[24m :");
-	ft_putendl("/*");
-	ft_putstr("** \033[7ma\033[27m");
+	ft_putstr_buffer("\n\033[4mStacks\033[24m :\n", 0);
+	ft_putstr_buffer("/*\n", 0);
+	ft_putstr_buffer("** \033[7ma\033[27m", 0);
 	while (i < ck->a->len)
 	{
-		ft_putstr(" ");
-		ft_putnbr(ck->a->tab[i]);
+		ft_putstr_buffer(" ", 0);
+		ft_putllnbr_buffer(ck->a->tab[i], 0);
 		++i;
 	}
-	ft_putchar('\n');
-	ft_putstr("** \033[7mb\033[27m");
+	ft_putchar_buffer('\n', 0);
+	ft_putstr_buffer("** \033[7mb\033[27m", 0);
 	i = 0;
 	while (i < ck->b->len)
 	{
-		ft_putstr(" ");
-		ft_putnbr(ck->b->tab[i]);
+		ft_putstr_buffer(" ", 0);
+		ft_putllnbr_buffer(ck->b->tab[i], 0);
 		++i;
 	}
-	ft_putchar('\n');
-	ft_putendl("*/\n");
+	ft_putchar_buffer('\n', 0);
+	ft_putendl_buffer("*/\n", 1);
 }
 
 void			checker_display_v_flag(t_checker *ck)
