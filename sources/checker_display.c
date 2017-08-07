3da82a0015c666a0ba1aebea37c30e35d598fb06
Mathis Pandelé <mapandel@student.42.fr>
2017-08-07 12:38:27 +0200

    checker_display.c 1.3 : Buffering

--- sources/checker_display.c
+++ sources/checker_display.c
@@ -6,7 +6,7 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/04 12:55:51 by mapandel          #+#    #+#             */
-/*   Updated: 2017/06/05 02:06:59 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/08/07 11:38:12 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,11 @@
 
 int		checker_display_usage(void)
 {
-	ft_putendl("Usage : ./checker [-lsv] list of integers");
-	ft_putendl("	l : Get rotations from log.txt");
-	ft_putendl("	v : Print the stacks + number of rotations + last move");
-	ft_putendl("	s (+ v) + [0 < speedtime < 5000000] : Choose speedtime \
-(microseconds)");
+	ft_putstr_buffer("Usage : ./checker [-lsv] list of integers\n", 1);
+	ft_putstr_buffer("	l : Get rotations from log.txt\n", 1);
+	ft_putstr_buffer("	v : Print the stacks + number of rotations", 1);
+	ft_putstr_buffer(" + last move\n", 1);
+	ft_putstr_buffer("	s (+ v) + [0 < speedtime < 5000000] : Choose speedtime (microseconds)\n", 1);
 	return (checker_display_result(ERROR));
 }
 
