90e945b132db7e15535c6de6977c86043a456d43
Mathis Pandelé <mapandel@student.42.fr>
2017-09-09 14:05:12 +0200

    push_swap_solve.c 1.7 : Init percentiles

--- sources/push_swap_solve.c
+++ sources/push_swap_solve.c
@@ -6,7 +6,7 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/05 04:23:18 by mapandel          #+#    #+#             */
-/*   Updated: 2017/08/29 06:08:32 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/09/09 13:48:07 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,12 +77,30 @@ void			push_swap_buffer_movement(t_push_swap *ps, int mv)
 		push_swap_buffer_movement2(ps, mv);
 }
 
+static void		push_swap_init_percentiles(t_push_swap *ps)
+{
+	ps->q12 = (int)(ft_tabpercentile(ps->a, 12));
+	ps->q19 = (int)(ft_tabpercentile(ps->a, 19));
+	ps->q25 = (int)(ft_tabpercentile(ps->a, 25));
+	ps->q32 = (int)(ft_tabpercentile(ps->a, 32));
+	ps->q37 = (int)(ft_tabpercentile(ps->a, 37));
+	ps->q44 = (int)(ft_tabpercentile(ps->a, 44));
+	ps->median = (int)(ft_tabmedian(ps->a));
+	ps->q57 = (int)(ft_tabpercentile(ps->a, 57));
+	ps->q62 = (int)(ft_tabpercentile(ps->a, 62));
+	ps->q69 = (int)(ft_tabpercentile(ps->a, 69));
+	ps->q75 = (int)(ft_tabpercentile(ps->a, 75));
+	ps->q82 = (int)(ft_tabpercentile(ps->a, 82));
+	ps->q87 = (int)(ft_tabpercentile(ps->a, 87));
+	ps->q94 = (int)(ft_tabpercentile(ps->a, 94));
+}
+
 t_push_swap		*push_swap_solver(t_push_swap *ps)
 {
 	remove("log.txt");
 	if (ps->l && (ps->fd = open("log.txt", O_CREAT | O_WRONLY, 0666)) == -1)
 		exit(-1);
-	ps->median = (int)(ft_tabmedian(ps->a));
+	push_swap_init_percentiles(ps);
 	while (!ft_tabisdescending(ps->a) || ps->b->len)
 	{
 		if (!ps->b->len && ((ps->a->len == 3
