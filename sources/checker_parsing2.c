326a4ea60637fa60811c0c442fc64be2005d73f7
Mathis PANDELE <mapandel@e2r5p21.42.fr>
2017-09-10 15:34:56 +0200

    checker_parsing2.c 1.1 : Fix

--- sources/checker_parsing2.c
+++ sources/checker_parsing2.c
@@ -6,7 +6,7 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/29 05:38:22 by mapandel          #+#    #+#             */
-/*   Updated: 2017/08/29 05:38:53 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/09/10 15:34:05 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,20 @@
 
 static t_checker	*checker_parsing_movements2(t_checker *ck)
 {
-	if (!ft_strncmp(ck->line, "rrb", 3) && (ck->rotation = RRB))
+	if (!ft_strncmp(ck->line, "rrb", 3) && ft_strlen(ck->line) == 3
+		&& (ck->rotation = RRB))
 		ft_tabrevrotate(ck->b);
-	else if (!ft_strncmp(ck->line, "rrr", 3) && ft_tabrevrotate(ck->a)
-		&& (ck->rotation = RRR))
+	else if (!ft_strncmp(ck->line, "rrr", 3) && ft_strlen(ck->line) == 3
+		&& ft_tabrevrotate(ck->a) && (ck->rotation = RRR))
 		ft_tabrevrotate(ck->b);
-	else if (!ft_strncmp(ck->line, "ra", 2) && (ck->rotation = RA))
+	else if (!ft_strncmp(ck->line, "ra", 2) && ft_strlen(ck->line) == 2
+		&& (ck->rotation = RA))
 		ft_tabrotate(ck->a);
-	else if (!ft_strncmp(ck->line, "rb", 2) && (ck->rotation = RB))
+	else if (!ft_strncmp(ck->line, "rb", 2) && ft_strlen(ck->line) == 2
+		&& (ck->rotation = RB))
 		ft_tabrotate(ck->b);
-	else if (!ft_strncmp(ck->line, "rr", 2) && ft_tabrotate(ck->a)
-		&& (ck->rotation = RR))
+	else if (!ft_strncmp(ck->line, "rr", 2) && ft_strlen(ck->line) == 2
+		&& ft_tabrotate(ck->a) && (ck->rotation = RR))
 		ft_tabrotate(ck->b);
 	else if (ck->line[0])
 		ck->error = 1;
@@ -33,18 +36,23 @@ static t_checker	*checker_parsing_movements2(t_checker *ck)
 
 t_checker			*checker_parsing_movements(t_checker *ck)
 {
-	if (!ft_strncmp(ck->line, "sa", 2) && (ck->rotation = SA))
+	if (!ft_strncmp(ck->line, "sa", 2) && ft_strlen(ck->line) == 2
+		&& (ck->rotation = SA))
 		ft_tabswap(ck->a);
-	else if (!ft_strncmp(ck->line, "sb", 2) && (ck->rotation = SB))
+	else if (!ft_strncmp(ck->line, "sb", 2) && ft_strlen(ck->line) == 2
+		&& (ck->rotation = SB))
 		ft_tabswap(ck->b);
-	else if (!ft_strncmp(ck->line, "ss", 2) && ft_tabswap(ck->a)
-		&& (ck->rotation = SS))
+	else if (!ft_strncmp(ck->line, "ss", 2) && ft_strlen(ck->line) == 2
+		&& ft_tabswap(ck->a) && (ck->rotation = SS))
 		ft_tabswap(ck->b);
-	else if (!ft_strncmp(ck->line, "pa", 2) && (ck->rotation = PA))
+	else if (!ft_strncmp(ck->line, "pa", 2) && ft_strlen(ck->line) == 2
+		&& (ck->rotation = PA))
 		ft_tabpush(ck->b, ck->a);
-	else if (!ft_strncmp(ck->line, "pb", 2) && (ck->rotation = PB))
+	else if (!ft_strncmp(ck->line, "pb", 2) && ft_strlen(ck->line) == 2
+		&& (ck->rotation = PB))
 		ft_tabpush(ck->a, ck->b);
-	else if (!ft_strncmp(ck->line, "rra", 3) && (ck->rotation = RRA))
+	else if (!ft_strncmp(ck->line, "rra", 3) && ft_strlen(ck->line) == 3
+		&& (ck->rotation = RRA))
 		ft_tabrevrotate(ck->a);
 	else
 		return (checker_parsing_movements2(ck));
