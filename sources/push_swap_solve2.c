b725074abb6aba7201ee4c574625c3aca885063c
Mathis Pandelé <mapandel@student.42.fr>
2017-09-12 06:09:49 +0200

    push_swap_solve2.c 1.2 : Optimised for 5 integers

--- sources/push_swap_solve2.c
+++ sources/push_swap_solve2.c
@@ -6,7 +6,7 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/29 05:46:22 by mapandel          #+#    #+#             */
-/*   Updated: 2017/09/09 13:41:42 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/09/12 05:58:49 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -93,28 +93,19 @@ static void		push_swap_solver3(t_push_swap *ps)
 
 void			push_swap_solver2(t_push_swap *ps)
 {
-	if (!ps->b->len && ps->a->len == 3
-		&& ps->a->tab[ps->a->len - 1] < ps->a->tab[ps->a->len - 2])
+	if (ps->a->len == 3
+		&& ps->a->tab[ps->a->len - 1] < ps->a->tab[ps->a->len - 2]
+		&& !ft_tabisdescending(ps->a))
 		push_swap_buffer_movement(ps, SA);
-	else if (!ps->b->len && ps->a->len <= 3)
+	else if (ps->a->len <= 3 && !ft_tabisdescending(ps->a))
 		push_swap_buffer_movement(ps, RA);
-	else if (ft_tabisincreasing(ps->a) && !ps->b->len)
-	{
-		while (ps->a->len > 1)
-		{
-			push_swap_buffer_movement(ps, RRA);
-			push_swap_buffer_movement(ps, PB);
-		}
-	}
 	else if ((ft_tabisdescending(ps->a)
 		|| !ps->a->len) && ft_tabisincreasing(ps->b) && (!ps->a->len
 		|| ps->a->tab[ps->a->len - 1] > ps->b->tab[ps->b->len - 1]))
 		push_swap_buffer_movement(ps, PA);
-	else if (ps->a->len <= 5 && ((ft_tabmin(ps->a) <= ps->median
-		&& ps->a->tab[ps->a->len - 1] > ps->median)
-		|| (ft_tabmin(ps->a) > ps->median
-		&& ps->a->tab[ps->a->len - 1] != ft_tabmin(ps->a))))
-		push_swap_buffer_movement(ps, RA);
+	else if (ps->a->len == 5 && (ft_tabmin(ps->a) == ps->a->tab[0]
+		|| ft_tabmin(ps->a) == ps->a->tab[1]))
+		push_swap_buffer_movement(ps, RRA);
 	else
 		push_swap_solver3(ps);
 }
